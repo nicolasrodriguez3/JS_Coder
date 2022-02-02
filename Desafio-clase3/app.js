4829a8fc470638d9308859e3bd5d7207db9764b5
Nicolas Rodriguez <nicolasrodriguez@outlook.com.ar>
2022-02-01 21:56:39 -0300

    desafio complementario clase 3

---
+++ Desafio-clase3/app.js
@@ -0,0 +1,61 @@
+/*let nombre = prompt("Ingrese su nombre");
+if (nombre != null && nombre != "") {
+	document.write("<h2>Hola " + nombre + "</h2>");
+}
+
+let multiplicador = parseInt(prompt("¿La tabla de multiplicar de que número desea conocer?"));
+let number = parseInt(prompt("¿Hasta que valor?"));
+
+for (let i = 0; i <= number / multiplicador; i++) {
+	document.write(`<p>${i} * ${multiplicador} = ${i * multiplicador}</p>`);
+}
+*/
+
+// Ejercicio 1: imprima la suma de todos los números impares entre 1 y 100
+let min = 1;
+let max = 100;
+let resultado = null;
+for (let i = min; i <= max; i++) {
+	if (i % 2 != 0) {
+		resultado += i;
+	}
+}
+console.log("La suma de los números impares entre " + min + " y " + max + " es " + resultado);
+
+function validar() {
+	if (palabra == undefined || palabra == "") {
+		alert("Mala onda");
+		palabra = "";
+	} else {
+		palabra = palabra.toLowerCase();
+	}
+}
+
+let palabra = prompt("Adivina mi nombre:");
+validar();
+let contador = 1;
+/*
+do {
+	if (palabra == "nico") {
+		alert("Sii!");
+		break;
+	}
+	if (palabra === "") break;
+	alert("Nop");
+	if (contador === 5) alert("Una ayudita: empieza con N");
+	contador++;
+	palabra = prompt("Adivina mi nombre:");
+	validar();
+} while (palabra != "nico");
+*/
+while (palabra != "nico") {
+	if (palabra === "") break;
+	alert("Nop");
+	if (contador === 3) alert("Una ayudita: empieza con N");
+	contador++;
+	palabra = prompt("Adivina mi nombre:");
+	validar();
+}
+if (palabra == "nico") {
+	alert("Sii!");
+}
