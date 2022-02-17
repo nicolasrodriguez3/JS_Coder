49bc5d6b2bb5d4094558af2fa00194a9ae95e59a
Nicolas Rodriguez <nicolasrodriguez@outlook.com.ar>
2022-02-17 16:24:09 -0300

    agregando drag and drop

--- Preentrega-clase7/app.js
+++ Preentrega-clase7/app.js
@@ -1,4 +1,17 @@
-const tareas = [];
+"use strict";
+const tareas = [
+	{
+		tarea: "prueba",
+		id: Date.now(),
+		completada: false,
+	},
+	{
+		tarea: "prueba true",
+		id: Date.now(),
+		completada: true,
+	}
+];
+mostrarTareas();
 
 const btn = document.getElementById("btn").addEventListener("click", (e) => {
 	e.preventDefault();
@@ -20,7 +33,7 @@ const btn = document.getElementById("btn").addEventListener("click", (e) => {
 	tarea.focus();
 });
 
-const mostrarTareas = function () {
+function mostrarTareas() {
 	// obtener el div donde se agregan las tareas y limpiar el contenido
 	const resultado = document.getElementById("resultado");
 	resultado.textContent = "";
@@ -30,22 +43,63 @@ const mostrarTareas = function () {
 		const li = document.createElement("li");
 		li.classList.add("tarea");
 		li.setAttribute("data-id", i.id);
+		li.draggable = true;
 		li.textContent = i.tarea;
 		ul.appendChild(li);
 	});
 
 	//agregar las tareas
 	resultado.appendChild(ul);
-};
+}
 
 //Tachar tareas
 resultado.addEventListener("click", (e) => {
-	e.target.classList.toggle("tachado");
-	const id = e.target.dataset.id;
-	const tachar = (id) => {
-		tareas.forEach((tarea) => {
-			if (tarea.id == id) tarea.completada ? (tarea.completada = false) : (tarea.completada = true);
-		});
-	};
-	tachar(id);
+	// Verificar si el elemento clickeado es uno de los LI
+	if (e.target.toString() == "[object HTMLLIElement]") {
+		e.target.classList.toggle("tachado");
+		const id = e.target.dataset.id;
+
+		actualizarEstadoTarea(id)
+	}
+})
+
+function actualizarEstadoTarea(id) {
+	tareas.forEach((tarea) => {
+		if (tarea.id == id) {
+			tarea.completada ? (tarea.completada = false) : (tarea.completada = true);
+		}
+	});
+}
+
+// obtener el div donde se borraran las tareas
+const papelera = document.getElementById("papelera");
+//identificar la tarea arrastrada
+let elementoArrastrado;
+
+resultado.addEventListener("dragstart", (e) => {
+	console.log(e);
+	elementoArrastrado = e.target;
+	e.target.style.opacity = 0.5;
+	elementoArrastrado.style.transform = "scale(.5)";
 });
+resultado.addEventListener("dragend", (e) => {
+	e.target.style.opacity = 1;
+	elementoArrastrado.style.transform = "scale(1)";
+});
+// evita el comportamiento por defecto que impide ejecutar el evento DROP
+papelera.addEventListener("dragover", (e) => {
+	e.preventDefault();
+	papelera.style.opacity = 0.5;
+});
+papelera.addEventListener("dragleave", (e) => {
+	papelera.style.opacity = 1;
+});
+
+papelera.addEventListener("drop", (e) => {
+	console.log(e);
+	//document.querySelector("#resultado>ul").removeChild(elementoArrastrado);
+});
+
+function agregarBorrador() {
+	tareas.length > 0 ? (papelera.style.display = "flex") : (papelera.style.display = "none");
+}
