3ab565f26c55aa435e9cb8154e072ba138042cb9
Nicolas Rodriguez <nicolasrodriguez@outlook.com.ar>
2022-02-28 17:16:25 -0300

    avanzando proyecto 2

--- Preentrega2/app.js
+++ Preentrega2/app.js
@@ -4,14 +4,19 @@ const parar = document.querySelector("#parar-temporizador");
 const sonido = new Audio("./sound/SD_ALERT.mp3");
 
 let timer;
+let tiempoInicial;
 let segundosRestantes;
-let funcionando = false;
+
 
 const actualizarTimer = () => {
 	if (segundosRestantes >= 0) {
-		resultado.textContent = `${Math.floor(segundosRestantes / 60)}:${segundosRestantes % 60}`;
+		resultado.textContent = `${Math.floor(segundosRestantes / 60)}:${
+			segundosRestantes % 60 < 10 ? "0" + (segundosRestantes % 60) : segundosRestantes % 60
+		}`;
 	} else {
-		resultado.textContent = `-${Math.floor(Math.abs(segundosRestantes) / 60)}:${Math.abs(segundosRestantes) % 60}`;
+		resultado.textContent = `-${Math.floor(Math.abs(segundosRestantes) / 60)}:${
+			Math.abs(segundosRestantes) < 10 ? "0" + Math.abs(segundosRestantes) : Math.abs(segundosRestantes) % 60
+		}`;
 	}
 };
 
@@ -20,7 +25,7 @@ const cuentaRegresiva = () => {
 	return setInterval(() => {
 		segundosRestantes -= 1;
 		actualizarTimer();
-		if (Math.abs(segundosRestantes) % 4 === 0 && segundosRestantes < 1) sonido.play();
+		if (segundosRestantes < 1 && Math.abs(segundosRestantes) % 4 === 0 && segundosRestantes > -30) sonido.play();
 	}, 1000);
 };
 
@@ -28,25 +33,40 @@ const cuentaRegresiva = () => {
 temporizador.addEventListener("submit", (e) => {
 	e.preventDefault();
 
-	const minutos = parseInt(document.querySelector("#minutos").value) || 0;
-	const segundos = parseInt(document.querySelector("#segundos").value) || 0;
-	segundosRestantes = minutos * 60 + segundos;
+	if (!timer) {
+		const minutos = parseInt(document.querySelector("#minutos").value) || 0;
+		const segundos = parseInt(document.querySelector("#segundos").value) || 0;
+		segundosRestantes = minutos * 60 + segundos;
+		tiempoInicial = segundosRestantes
 
-	actualizarTimer();
+		resultado.classList.remove("parpadear");
+		actualizarTimer();
 
-	timer = cuentaRegresiva();
+		timer = cuentaRegresiva();
+		parar.addEventListener("click", pararTemporizador);
+		temporizador[2].textContent = "Reset"
+	}else{
+		resultado.textContent = ""
+		temporizador[2].textContent = "Iniciar"
+		pararTemporizador()
+		resultado.classList.remove("parpadear");
+		parar.textContent = "Parar";
+		
+	}
 });
 
 // Parar temporizador
-parar.addEventListener("click", () => {
+function pararTemporizador() {
 	// si existe el temporizador lo pausamos
 	if (timer) {
 		clearInterval(timer);
-		resultado.classList.add("parpadear");
 		//removemos el id del temporizador
 		timer = null;
+		resultado.classList.add("parpadear");
+		parar.textContent = "Reanudar";
 	} else {
 		timer = cuentaRegresiva();
 		resultado.classList.remove("parpadear");
+		parar.textContent = "Parar";
 	}
-});
+}
